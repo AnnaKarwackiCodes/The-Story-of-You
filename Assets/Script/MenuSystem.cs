b2bc33452e93cfa5bd97b8b002fd6bb013d9ac52
avk9238 <avk9238@rit.edu>
2016-11-11 21:20:34 -0500

    menu system is now finished

--- Assets/Script/MenuSystem.cs
+++ Assets/Script/MenuSystem.cs
@@ -3,6 +3,11 @@ using System.Collections;
 
 public class MenuSystem : MonoBehaviour {
 
+    [SerializeField]
+    GameObject MainMenu;
+    [SerializeField]
+    GameObject HowMenu;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -12,4 +17,23 @@ public class MenuSystem : MonoBehaviour {
 	void Update () {
 	
 	}
+
+    //when you press the start game button on the main menu it will load in the game
+    public void StartGame()
+    {
+        Application.LoadLevel("Apartment");
+    }
+
+    //when you press the how to play button on the main menu will switch the game to this screen
+    public void HowToMenu()
+    {
+        MainMenu.SetActive(false);
+        HowMenu.SetActive(true);
+    }
+    //back button on the how to play screen
+    public void goBack()
+    {
+        HowMenu.SetActive(false);
+        MainMenu.SetActive(true);
+    }
 }
